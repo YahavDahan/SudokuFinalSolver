89a1819ac08943eb5cc91e62b8afb7cf7fb087c6
Yahav Dahan <yahavd258@gmail.com>
2022-02-03 14:46:56 +0200

    creates Exceptions classes

---
+++ Sudoku/Exceptions/AsciiCharacterOutOfRangeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Exceptions
+{
+    public class AsciiCharacterOutOfRangeException : Exception
+    {
+        public AsciiCharacterOutOfRangeException(string message)
+        : base(message) { }
+
+        //public override string ToString()
+        //{
+        //    return Message;
+        //}
+    }
+}
